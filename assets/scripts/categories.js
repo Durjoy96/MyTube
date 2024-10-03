05998b7f0df06e3895fd42164c69b3a0335018b8
Durjoy96 <mddurjoy853@gmail.com>
2024-10-03 12:13:10 +0600

    no videos msg added

--- assets/scripts/categories.js
+++ assets/scripts/categories.js
@@ -27,5 +27,16 @@ function setEventListener() {
 async function getCategoriesVideos(id) {
     const response = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
     const data = await response.json();
-    showVideos(data.category);
+    if(data.category.length > 0) {
+        showVideos(data.category);
+    } else {
+        document.getElementById("videosContainer").innerHTML = `
+        <div class="max-w-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/4">
+        <div class="flex flex-col items-center gap-3">
+        <img class="max-w-32" src="assets/img/Icon.png" alt="">
+        <p class="text-2xl font-bold text-gray-800 text-center">Oops!! Sorry, There is no content here</p>
+        </div>
+        </div>
+        `;
+    };
 };
